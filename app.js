d8bd326c77a5986a5b70285beabd63d9b1281be9
vishw28 <vishwvajaria@gmail.com>
2016-12-30 04:07:12 -0500

    view(homepage)

--- app.js
+++ app.js
@@ -1,3 +1,18 @@
 /**
  * Created by Vishw on 12/30/2016.
  */
+
+(function () {
+    'use strict';
+
+    var app = angular.module('quizApp',['ngRoute']);
+
+    app.config(['$routeProvider',function ($routeProvider) {
+        $routeProvider
+            .when('/',{
+                templateUrl:'views/home.html',
+                controller:'MainController'
+            })
+            .otherwise('/');
+    }])
+})();
---
+++ controllers/main.controller.js
@@ -0,0 +1,14 @@
+/**
+ * Created by Vishw on 12/30/2016.
+ */
+
+(function () {
+    'use strict';
+    angular.module('quizApp')
+        .controller('MainController',MainController);
+
+    function MainController($scope) {
+
+    }
+
+})();
