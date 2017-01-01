f25e6989f5679fe7dfe20ec76a01cbf22b86c5bf
vishw28 <vishwvajaria@gmail.com>
2016-12-31 22:45:19 -0500

    marking the quiz

--- services/quizMatrics.service.js
+++ services/quizMatrics.service.js
@@ -7,22 +7,39 @@
 
     angular.module('quizApp')
         .factory('QuizMatrics',QuizMatrics);
-    
+
+    QuizMatrics.$inject = ['QuizService'];
+
     function QuizMatrics() {
         var quizObj = {
             quizActive: false,
-            chnageState: changeState
+            chnageState: changeState,
+            markQuiz: markQuiz,
+            correctAnswers: [],
+            numCorrect: 0
         };
 
         return quizObj;
         
-        function chnageState(metric,state) {
+        function changeState(metric,state) {
             if(metric == "quiz"){
                 quizObj.quizActive = state;
             }else if(metric == "result"){
                 quizObj.resultActive = state;
             }else{
                 return false;
+            }
+        }
+        
+        function markQuiz() {
+            quizObj.correctAnswers = QuizService.correctAnswers;
+            for(var i=0 ; i < QuizService.quizQuestions.length; i++){
+                if(QuizService.correctAnswers[i] === QuizService.quizQuestions[i].selected){
+                    QuizService.quizQuestions[i].correct = true;
+                    quizObj.numCorrect++;
+                }else {
+                    QuizService.quizQuestions[i].correct = false;
+                }
             }
         }
     }
